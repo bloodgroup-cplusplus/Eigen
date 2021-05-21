344302863fd63fa4a93b337b4f9d8c9e231e941b
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-05-22 00:58:36 +0530

    Create eigenvalueandeigenvector.cpp

---
+++ eigenvalueandeigenvector.cpp
@@ -0,0 +1,20 @@
+// this code is used to find an eigen value and eigen vector for a 2*2 float matrix
+
+#include<iostream>
+#include<Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+    Matrix2f A;
+    A<<1,2,2,3;
+    cout<<"The given matrix is A:\n"<<A<<endl;
+    SelfAdjointEigenSolver<Matrix2f> eigensolver(A);
+    if(eigensolver.info()!=Success) abort();
+    cout<<"The eigenvalues of A are:\n"<<eigensolver.eigenvalues()<<endl;
+    cout<<"Here's a matrix whose columns are eigenvectors of A\n corresponding to these eigenvalues:\n"
+      <<eigensolver.eigenvectors()<<endl;
+    return 0;
+}
