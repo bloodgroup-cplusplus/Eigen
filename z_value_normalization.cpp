0af67b079fb2f3f744626ce90470135fefafcbe6
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-05-28 18:30:11 +0530

    Create z_value_normalization.cpp

---
+++ z_value_normalization.cpp
@@ -0,0 +1,11 @@
+// For more about z_value_normalization read the article here https://www.codecademy.com/articles/normalization
+
+#include<iostream>
+#include<eigen3/Eigen/Dense>
+
+int main()
+{
+		return 0;
+}
+
+
