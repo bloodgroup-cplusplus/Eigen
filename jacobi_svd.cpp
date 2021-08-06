3de0be55356e43f9257009129f63e518667c8974
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-08-06 12:25:32 +0530

    Update jacobi_svd.cpp

--- jacobi_svd.cpp
+++ jacobi_svd.cpp
@@ -7,6 +7,9 @@ int main()
         Eigen::JacobiSVD<Eigen::MatrixXf> svd(m,Eigen::ComputeThinU | Eigen::ComputeThinV);
         std::cout<<"The singularvaues using jaboi svd are "<<std::endl;
         std::cout<<svd.singularValues()<<std::endl;
+        std::cout<<"Its left singular vectors are the columns of the thin U matrix:"<<std::endl<<svd.matrixU()<<std::endl;
+        std::cout<<"Its right singular vectors are the columns of the thin V matrix:"<<std::endl<<svd.matrixV()<<std::endl;
+        
 
         return 0;
 }
