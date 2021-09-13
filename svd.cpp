762ae41f5ea461d0cce14685014e31ca0bc1db26
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-09-13 23:02:20 +0530

    Update svd.cpp

--- svd.cpp
+++ svd.cpp
@@ -10,3 +10,13 @@ int main()
 	std::cout<<"The least-squares solution is:\n"<<A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinv).solve(b)<<std::endl;
 	return 0;;
 }
+
+/*The svd algorithm factorizes a matrix into one matrix with orthogonal columns on with orthogonal rows (along with a diagonal matrix,
+which contains the relative importance of each factor).
+
+We would cleaerly expect that the values that appear most frequently in one topic would appear less frequently in the other.
+otherwise that word wouldn't make a good choice to seperate out the two topics. Therefore, we expect the topics to be orthogonal
+*/
+
+
+
