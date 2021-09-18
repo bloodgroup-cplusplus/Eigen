226cc389f2edc5fc7d7dced0837e2922c1fff9ea
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-09-18 23:32:58 +0530

    Update eigenvalue.cpp

--- eigenvalue.cpp
+++ eigenvalue.cpp
@@ -8,7 +8,7 @@ int main()
 {
     Eigen::MatrixXd A=Eigen::MatrixXd::Random(6,6);
     std::cout<<"here is a random 6*6 matrix, A:"<<std::endl;
-    std::cout<<A<<std::endl;
+    std::cout<<A<<std::endl;;;
     Eigen::EigenSolver<Eigen::MatrixXd> es(A);
     std::cout<<"The eigenvalues of A are :"<<std::endl;
     std::cout<<es.eigenvalues()<<std::endl;
